b704ce38e352e0e1fd4fefc17e029d42e73796eb
Sebastian Gonzalez <sgonzalez11@gmail.com>
2016-05-24 23:24:30 -0300

    type scripts form to input and confirm data. app component modified to include the other ts. gitignore modified

---
+++ dev/bindings/input.component.ts
@@ -0,0 +1,48 @@
+import {Component, EventEmitter} from 'angular2/core';
+
+@Component({
+    selector: 'my-input',
+    template: `
+        <h1>Your details, please</h1>
+        <div>
+            <label for="name">Your name:</label>
+            <input type="text" id="name" [(ngModel)]= "myself.name" (keyup)="onKeyup()">
+        </div>
+        <div>
+            <label for="name">Your age:</label>
+            <input type="text" id="name" [(ngModel)]= "myself.age" (keyup)="onKeyup()">
+        </div>
+        <br>
+        <div>Filled out: {{isFilled ? 'Yes' : 'No'}}</div>
+        <div>Valid: {{isValid ? 'Yes' : 'No'}}</div>
+        <br>
+        <button [disabled]="!isValid" (click)="onSubmit()">Submit</button>
+    `,
+    inputs: ['myself'],
+    outputs: ['submitted']
+})
+
+export class InputComponent {
+    myself = {name: "", age: ""};
+    isFilled = false;
+    isValid = false;
+    submitted = new EventEmitter<{name: string, age: string}>();
+
+    onKeyup() {
+        if (this.myself.name != '' && this.myself.age != '') {
+            this.isFilled = true;
+        } else {
+            this.isFilled = false;
+        }
+
+        if (this.myself.name != '' && /^\d+$/.test(this.myself.age)) {
+            this.isValid = true;
+        } else {
+            this.isValid = false;
+        }
+    }
+
+    onSubmit() {
+        this.submitted.emit(this.myself);
+    }
+}
